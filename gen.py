e01f047e46ec247fbaa9511c1023fb52d6b5cb5a
Cassie <124466161+cassie4harter@users.noreply.github.com>
2023-05-25 19:24:27 -0500

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,17 +1,50 @@
 import random
+import string
 import csv
+import boto3
 
-# Create a list of random integers
-integers = [random.randint(1, 10) for _ in range(10000000)]
+# AWS S3 configuration
+bucket_name = 'random-data-cassie'
+output_file = 'source.csv'
 
-# Create a list of random strings
-strings = [random.choice("abcdefghijklmnopqrstuvwxyz0123456789") for _ in range(10000000)]
+# Generate a random string of given length
+def generate_random_string(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for _ in range(length))
 
-# Create a list of tuples
-tuples = [(i, s1, s2) for i, s1, s2 in zip(integers, strings, strings)]
+# Generate CSV data
+def generate_csv_data(num_rows):
+    data = []
+    for i in range(1, num_rows+1):
+        row = [
+            i,                             # id (auto-incrementing)
+            random.randint(1, 10),         # integer1 (random number 1-10)
+            generate_random_string(32),    # string1 (random characters of length 1-32)
+            generate_random_string(32)     # string2 (random characters of length 1-32)
+        ]
+        data.append(row)
+    return data
 
-# Write the tuples to a CSV file
-with open("source.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["id", "integer1", "string1", "string2"])
-    writer.writerows(tuples)
+# Write CSV data to a file
+def write_to_csv(data):
+    with open(output_file, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow(['id', 'integer1', 'string1', 'string2'])  # Header row
+        writer.writerows(data)
+
+# Upload file to S3 bucket
+def upload_to_s3(filename):
+    s3 = boto3.client('s3')
+    s3.upload_file(filename, bucket_name, filename)
+
+if __name__ == '__main__':
+    desired_file_size = 1024 * 1024 * 1024  # 1GB
+    row_size = 1 + 4 + 32 + 32  # Size of one row (in bytes)
+
+    num_rows = desired_file_size // row_size
+
+    data = generate_csv_data(num_rows)
+    write_to_csv(data)
+    upload_to_s3(output_file)
+
+    print(f'{output_file} created and uploaded to {bucket_name} bucket.')
